096aeb099f46f0a874bcff4c16c786538376f7f6
narek <narekhunanyan0497@gmail.com>
2025-11-20 21:29:03 +0400

    fix main ts

--- src/main.ts
+++ src/main.ts
@@ -5,17 +5,9 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  const allowedOrigins = [
-    process.env.FRONTEND_ORIGIN, // DO
-    process.env.WEBAPP_URL, // Vercel
-    'http://localhost:5173', // Vite dev
-  ].filter(Boolean); // убираем undefined/null
-
   app.enableCors({
-    origin: allowedOrigins,
+    origin: process.env.FRONTEND_ORIGIN || true,
     credentials: true,
-    methods: ['GET', 'POST', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
   });
 
   const port = process.env.PORT || 3000;
