7dd10d693397b071de2c952c8859c95355a0dca2
narek <narekhunanyan0497@gmail.com>
2025-12-14 16:27:51 +0400

    add chang tickets

--- src/tickets/tickets.module.ts
+++ src/tickets/tickets.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { TicketsController } from './tickets.controller';
 import { TicketsService } from './tickets.service';
 import { PrismaService } from '../prisma/prisma.service';
+import { AuthModule } from '../auth/auth.module';
 
 @Module({
   controllers: [TicketsController],
   providers: [TicketsService, PrismaService],
-})
+  imports: [AuthModule],})
 export class TicketsModule {}
